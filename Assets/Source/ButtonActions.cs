eda693cf8f6048b0890ff773a8ad5fdd0a7d07d1
Megawats777 <jedsemilla@gmail.com>
2016-08-08 16:25:29 -0400

    Added funtionality to restart and quit buttons on the game over screen

---
+++ Assets/Source/ButtonActions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonActions : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Open a level
+    public void openLevel(string levelName)
+    {
+        SceneManager.LoadSceneAsync(levelName);
+    }
+
+    // Quit the application
+    public void quitApplication()
+    {
+        Application.Quit();
+    }
+}
