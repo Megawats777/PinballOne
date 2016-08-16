f19209514834f9867bb40db5c4825468563a48bb
Megawats777 <jedsemilla@gmail.com>
2016-08-15 21:28:35 -0400

    Started work on the level select screen

--- Assets/Source/MainMenuManager.cs
+++ Assets/Source/MainMenuManager.cs
@@ -13,10 +13,22 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     private Animator welcomeBackgroundPanelAnimator;
 
+    // Level select screen properties
+    [Header("Level Select Screen Properties"), SerializeField]
+    private Animator levelSelectPanelAnimator;
+
+    // Screen groups
+    [Header("Screen Groups"), SerializeField]
+    private GameObject welcomeScreenGroup;
+
+    [SerializeField]
+    private GameObject levelSelectScreenGroup;
+
     // Use this for initialization
     void Start()
     {
-
+        // Enable the welcome screen group
+        welcomeScreenGroup.SetActive(true);
     }
 
     // Update is called once per frame
@@ -35,12 +47,15 @@ public class MainMenuManager : MonoBehaviour
         welcomeBackgroundPanelAnimator.SetBool("isSlidingOut", true);
 
         // Slide in the level select screen elements
+        levelSelectPanelAnimator.SetBool("isIdle", false);
+        levelSelectPanelAnimator.SetBool("isSlidingOut", false);
     }
 
     // Open the welcome screen
     public void openWelcomeScreen()
     {
         // Slide out the level select screen elements
+        levelSelectPanelAnimator.SetBool("isSlidingOut", true);
 
         // Slide in the welcome screen elements
         gameTitleAnimator.SetBool("isSlidingOut", false);
