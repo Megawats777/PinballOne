e474907980e8a757585ef09b6ccb0af071b33cf0
Megawats777 <jedsemilla@gmail.com>
2016-08-17 21:56:20 -0400

    Added functionality where when you hover over a menu button it will play a sound

---
+++ Assets/Source/HoveredButtonActions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoveredButtonActions : MonoBehaviour
+{
+
+    /*--Properties of the class--*/
+
+    // Reference to the button's audio source component
+    private AudioSource buttonAudioSource;
+
+    // Called before start
+    public void Awake()
+    {
+        // Get the button's audio source component
+        buttonAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Play a sound
+    public void playHoveredSound()
+    {
+        buttonAudioSource.Play();
+    }
+}
